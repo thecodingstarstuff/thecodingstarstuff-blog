64661ef402eb465c868d8bdd8d2f288be025ea24
Rodrigo E. Gimenez <rodrigog83@gmail.com>
2020-10-18 11:51:31 +0200

    Remove photo and rename blog

--- pages/index.js
+++ pages/index.js
@@ -22,11 +22,7 @@ export default function Home({ allPostsData }) {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>Hi! I'm Rodrigo. I'm a Back End Developer</p>
-        <p>
-          (This is a sample website - you’ll be building a site like this on{" "}
-          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
-        </p>
+        <p>The Coding Starstuff Blog - Coming Soon</p>
       </section>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
         <h2 className={utilStyles.headingLg}>Blog</h2>
