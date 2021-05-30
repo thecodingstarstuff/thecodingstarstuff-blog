3e116eca4c0485a0c86b094cd3c573dad02fc926
Rodrigo Gimenez <rodrigog83@gmail.com>
2021-05-30 14:19:07 -0300

    Add syntax highlighting to code blocks

--- components/layout.tsx
+++ components/layout.tsx
@@ -4,71 +4,71 @@ import utilStyles from "../styles/utils.module.scss";
 import Link from "next/link";
 
 const name = "Rodrigo E. Gimenez";
-export const siteTitle = "The Coding Star-Stuff";
+export const siteTitle = "The Coding Starstuff";
 
 export default function Layout({
-  children,
-  home,
-}: {
-  children: React.ReactNode;
-  home?: boolean;
+                                   children,
+                                   home,
+                               }: {
+    children: React.ReactNode;
+    home?: boolean;
 }) {
-  return (
-    <div className={styles.container}>
-      <Head>
-        {/* <link rel="icon" href="/favicon.ico" /> */}
-        <meta
-          name="description"
-          content="Learn how to build a personal website using Next.js"
-        />
-        <meta
-          property="og:image"
-          content={`https://og-image.now.sh/${encodeURI(
-            siteTitle
-          )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
-        />
-        <meta name="og:title" content={siteTitle} />
-        <meta name="author" content="Rodrigo E. Gimenez" />
-        <meta name="twitter:card" content="summary_large_image" />
-      </Head>
-      <header className={styles.header}>
-        {home ? (
-          <>
-            <div className={utilStyles.quote}>
-              <blockquote>
-                <p className={utilStyles.lightText}>
-                  "The cosmos is also within us, we're made of star stuff,"
-                </p>
-                <cite><a href="https://en.wikipedia.org/wiki/Carl_Sagan">Carl Sagan</a></cite>
-              </blockquote>
-            </div>
-            <h1 className={utilStyles.heading2Xl}>{siteTitle}</h1>
-          </>
-        ) : (
-          <>
-            <h2 className={utilStyles.headingLg}>
-              <Link href="/">
-                <a className={utilStyles.colorInherit}>{siteTitle}</a>
-              </Link>
-            </h2>
-          </>
-        )}
-      </header>
-      {!home && (
-        <div className={styles.backToHome}>
-          <Link href="/">
-            <a>← Back to home</a>
-          </Link>
+    return (
+        <div className={styles.container}>
+            <Head>
+                {/* <link rel="icon" href="/favicon.ico" /> */}
+                <meta
+                    name="description"
+                    content="Learn how to build a personal website using Next.js"
+                />
+                <meta
+                    property="og:image"
+                    content={`https://og-image.now.sh/${encodeURI(
+                        siteTitle
+                    )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
+                />
+                <meta name="og:title" content={siteTitle}/>
+                <meta name="author" content="Rodrigo E. Gimenez"/>
+                <meta name="twitter:card" content="summary_large_image"/>
+            </Head>
+            <header className={styles.header}>
+                {home ? (
+                    <>
+                        <div className={utilStyles.quote}>
+                            <blockquote>
+                                <p className={utilStyles.lightText}>
+                                    "The cosmos is also within us, we're made of star stuff,"
+                                </p>
+                                <cite><a href="https://en.wikipedia.org/wiki/Carl_Sagan">Carl Sagan</a></cite>
+                            </blockquote>
+                        </div>
+                        <h1 className={utilStyles.heading2Xl}>{siteTitle}</h1>
+                    </>
+                ) : (
+                    <>
+                        <h2 className={utilStyles.headingLg}>
+                            <Link href="/">
+                                <a className={utilStyles.colorInherit}>{siteTitle}</a>
+                            </Link>
+                        </h2>
+                    </>
+                )}
+            </header>
+            {!home && (
+                <div className={styles.backToHome}>
+                    <Link href="/">
+                        <a>← Back to home</a>
+                    </Link>
+                </div>
+            )}
+            <main>{children}</main>
+            {!home && (
+                <div className={styles.backToHome}>
+                    <Link href="/">
+                        <a>← Back to home</a>
+                    </Link>
+                </div>
+            )}
         </div>
-      )}
-      <main>{children}</main>
-      {!home && (
-        <div className={styles.backToHome}>
-          <Link href="/">
-            <a>← Back to home</a>
-          </Link>
-        </div>
-      )}
-    </div>
-  );
+    );
 }
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,9 @@
-import { AppProps } from "next/dist/next-server/lib/router/router";
+import {AppProps} from "next/dist/next-server/lib/router/router";
 import "../styles/global.scss";
+import 'highlight.js/styles/github-dark.css';
 
-const App = ({ Component, pageProps }: AppProps) => {
-  return <Component {...pageProps} />;
+const App = ({Component, pageProps}: AppProps) => {
+    return <Component {...pageProps} />;
 };
 
 export default App;
